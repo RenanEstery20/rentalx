938d4defcb5156ee98054a542b3f24aa44dcfa96
RenanEstery20 <renanestery1@gmail.com>
2022-03-02 15:48:14 -0300

    ListCategories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,3 +1,13 @@
-import { Category } from "../../model/Category";
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-console.log();
+class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+  handle(req: Request, res: Response): Response {
+    const all = this.listCategoriesUseCase.execute();
+
+    return res.json(all);
+  }
+}
+
+export { ListCategoriesController };
